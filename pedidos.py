fc4a3a5ad0852c53bc36a9b895ef9ec48b470bfb
vinicius-akio <viniciusakiogit@gmail.com>
2026-06-11 11:07:39 -0300

    melhorei as validações de status

--- pedidos.py
+++ pedidos.py
@@ -1,6 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 
 class Pedidos (BaseModel):
-    cliente_id: int
+    cliente_id: int = Field (ge = 1)
     data_pedido: str
     status: str
+
+    @field_validator ("status")
+    def validacao_status (cls, status):
+        
+        status = status.title().strip()
+
+        if status not in ['Entregue', 'Enviado', 'Processando']:
+            raise ValueError('Status do pedido inválido.')
+
+        return status
